f0f466468e15b4cd4d1363c7e9ec1937ba462b1e
Soham Saha <90675152+DeadlySOHAM@users.noreply.github.com>
2024-12-01 16:03:59 +0530

    47 : Permutations II

---
+++ src/Permutations2/Permutations.java
@@ -0,0 +1,12 @@
+package Permutations2;
+
+// 47
+
+class Permutations2{
+   public static void main(String[] args){
+      Solution s = new Solution();
+      System.out.println(s.permute(new int[]{0}));
+      System.out.println(s.permute(new int[]{0,1}));
+      System.out.println(s.permute(new int[]{1,2,3}));
+   }
+}
---
+++ src/Permutations2/Solution.java
@@ -0,0 +1,34 @@
+package Permutations2;
+
+import java.util.*;
+import java.util.stream.*;
+
+public class Solution {
+   
+   public List<List<Integer>> permute(int[] nums) {
+      TreeSet<ArrayList<Integer>> result = new TreeSet<ArrayList<Integer>>((a,b)->a.toString().compareTo(b.toString()));
+      findPermutation(nums,0,result);
+      return result.stream().collect(Collectors.toList());
+   }
+
+   public static void findPermutation(int[] nums, int index, TreeSet<ArrayList<Integer>> result) {
+      if(index == nums.length-1){
+         ArrayList<Integer> temp = new ArrayList<Integer>(nums.length);
+         for(int i=0;i<nums.length;i++)
+            temp.add(nums[i]);
+         result.add(temp);
+         return;
+      }
+      for(int swapI = index; swapI<nums.length; swapI++) {
+         swap(nums,index,swapI);
+         findPermutation(nums,index+1,result);
+         swap(nums,index,swapI);
+      }
+   }
+
+   private static void swap(int[] ch, int dx, int dy){
+      int temp = ch[dx];
+      ch[dx] = ch[dy];
+      ch[dy] = temp;
+   }
+}
