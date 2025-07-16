3281faeb138aadb24e5d7526986a7bdcc48979d5
Soham Saha <90675152+DeadlySOHAM@users.noreply.github.com>
2025-07-16 21:59:32 +0530

    410. Split Array Largest Sum

---
+++ src/SplitArrayLargestSum/Solution.java
@@ -0,0 +1,44 @@
+package SplitArrayLargestSum;
+
+public class Solution {
+
+   public int splitArray(int[] nums, int k) {
+      int lower = Integer.MAX_VALUE, upper = 0, max = Integer.MAX_VALUE;
+      for(int element : nums) {
+         upper += element;
+         lower = Math.min(element, lower);
+         max = Math.max(max, element);
+      }
+      if(k==1) return upper;
+      if(k==nums.length) return max;
+      int ans = Integer.MIN_VALUE;
+      while(lower<=upper) {
+         int mid = (lower+upper) >> 1;
+         int maxTime = maxTime(nums,k,mid);
+         if(maxTime == Integer.MIN_VALUE) lower = mid+1;
+         else {
+            ans = mid;
+            upper = mid-1;
+         }
+      }
+      return ans;
+   }
+
+   public int maxTime(int[] nums, int k, int limit) {
+      int _limit = 0, maxTime = Integer.MIN_VALUE;
+      for(int i=0; i<nums.length && k>0; i++) {
+         _limit += nums[i];
+         if(_limit<=limit) {
+            maxTime = Math.max(_limit,maxTime);
+            if(i==nums.length-1) return maxTime;
+         } else {
+            _limit = 0;
+            --i;
+            --k;
+         }
+      }
+      return Integer.MIN_VALUE;
+   }
+
+   
+}
---
+++ src/SplitArrayLargestSum/SplitArrayLargestSum.java
@@ -0,0 +1,16 @@
+package SplitArrayLargestSum;
+
+// 410
+
+public class SplitArrayLargestSum {
+   public static void main(String args[]) {
+      Solution s = new Solution();
+      System.out.println(s.splitArray(new int[] { 5, 10, 30, 20, 15 }, 3)); // 35
+      System.out.println(s.splitArray(new int[] { 10, 20, 30, 40 }, 2)); // 60
+      System.out.println(s.splitArray(new int[] { 100, 200, 300, 400 }, 1)); // 1000
+      System.out.println(s.splitArray(new int[] { 4, 2, 22, 8, 2, 11, 19, 13 }, 3)); // 32
+      System.out.println(s.splitArray(new int[] { 10, 5, 20, 25, 17, 23, 2, 9, 4, 13 }, 7)); // 25
+      System.out.println(s.splitArray(new int[] { 7, 2, 5, 10, 8 }, 2)); // 18
+      System.out.println(s.splitArray(new int[] { 1, 2, 3, 4, 5 }, 2)); // 9
+   }
+}
