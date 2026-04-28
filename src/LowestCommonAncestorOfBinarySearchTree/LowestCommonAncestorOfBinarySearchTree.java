41dd95d06a6fc262f52f7b74649957b0998d2ba7
Soham Saha <90675152+DeadlySOHAM@users.noreply.github.com>
2026-04-28 20:29:57 +0530

    235. Lowest Common Ancestor of a Binary Search Tree

---
+++ src/LowestCommonAncestorOfBinarySearchTree/LowestCommonAncestorOfBinarySearchTree.java
@@ -0,0 +1,9 @@
+package LowestCommonAncestorOfBinarySearchTree;
+
+// 235. Lowest Common Ancestor of a Binary Search Tree
+
+public class LowestCommonAncestorOfBinarySearchTree {
+   public static void main(String[] args) {
+      // Already done in web
+   }
+}
---
+++ src/LowestCommonAncestorOfBinarySearchTree/Solution2.java
@@ -0,0 +1,12 @@
+package LowestCommonAncestorOfBinarySearchTree;
+
+public class Solution2 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while(root!=null) {
+            if(root.val>p.val && root.val>q.val) root = root.left;
+            else if(root.val<p.val && root.val<q.val) root = root.right;
+            else return root;
+        }
+        return root;
+    }
+}
